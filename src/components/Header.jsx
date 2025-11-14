1fae92a373f1c910cf56d739a04b73ce04e2add5
allan012 <allanheller4@gmail.com>
2025-11-13 20:43:13 -0500

    I have created my first component called <header>

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <header>
+            <img src="src/assets/react-logo-xs.png" alt="logo react" />
+            <h1>React fundamentals</h1>
+            <p>
+                fundamentals react that you need to use for developing whatever app with this famous library
+            </p>
+        </header>
+    )
+}
+
+
+export default Header
